6a6e9144865ad8d1d459fcba49bf119827fe3895
ballinhuang <x3562151@gmail.com>
2019-03-21 08:37:56 +0000

    printConnection

--- HW-1/hw1.cpp
+++ HW-1/hw1.cpp
@@ -24,7 +24,24 @@ void ipv4_hex2addr(struct in_addr hex_addr, char addr[INET_ADDRSTRLEN])
     inet_ntop(AF_INET, &hex_addr, addr, INET_ADDRSTRLEN * sizeof(char));
 }
 
-void printIPV4Connections(string ipv4_path)
+void ipv6_hex2addr(char str_addr[33], char addr[INET6_ADDRSTRLEN])
+{
+    struct in6_addr hex_addr;
+    sscanf(str_addr, "%08X%08X%08X%08X",
+           &hex_addr.s6_addr32[0], &hex_addr.s6_addr32[1], &hex_addr.s6_addr32[2], &hex_addr.s6_addr32[3]);
+    inet_ntop(AF_INET6, &hex_addr, addr, INET6_ADDRSTRLEN * sizeof(char));
+}
+
+void printConnection(string type, string src_ip, int src_port, string dis_ip, int dis_port, string inode)
+{
+
+    cout << left << setw(6) << type
+         << setw(25) << src_ip + ":" + porthex2string(src_port)
+         << setw(25) << dis_ip + ":" + porthex2string(dis_port);
+    cout << inode << endl;
+}
+
+void getIPV4Connections(string ipv4_path)
 {
     /*
         sl  local_address rem_address   st tx_queue rx_queue tr tm->when retrnsmt   uid  timeout inode
@@ -45,12 +62,7 @@ void printIPV4Connections(string ipv4_path)
         {
             ipv4_hex2addr(src_addr, src_ip);
             ipv4_hex2addr(dis_addr, dis_ip);
-
-            cout << left << setw(6) << "tcp"
-                 << setw(25) << string(src_ip) + ":" + porthex2string(src_port)
-                 << setw(25) << string(dis_ip) + ":" + porthex2string(dis_port);
-
-            cout << inode << endl;
+            printConnection("tcp", string(src_ip), src_port, string(dis_ip), dis_port, string(inode));
         }
     }
     else
@@ -59,15 +71,7 @@ void printIPV4Connections(string ipv4_path)
     }
 }
 
-void ipv6_hex2addr(char str_addr[33], char addr[INET6_ADDRSTRLEN])
-{
-    struct in6_addr hex_addr;
-    sscanf(str_addr, "%08X%08X%08X%08X",
-           &hex_addr.s6_addr32[0], &hex_addr.s6_addr32[1], &hex_addr.s6_addr32[2], &hex_addr.s6_addr32[3]);
-    inet_ntop(AF_INET6, &hex_addr, addr, INET6_ADDRSTRLEN * sizeof(char));
-}
-
-void printIPV6Connections(string ipv6_path)
+void getIPV6Connections(string ipv6_path)
 {
     /*
         sl  local_address                         remote_address                        st tx_queue rx_queue tr tm->when retrnsmt   uid  timeout inode
@@ -88,12 +92,7 @@ void printIPV6Connections(string ipv6_path)
         {
             ipv6_hex2addr(src_addr_str, src_ip);
             ipv6_hex2addr(dis_addr_str, dis_ip);
-
-            cout << left << setw(6) << "tcp6"
-                 << setw(25) << string(src_ip) + ":" + porthex2string(src_port)
-                 << setw(25) << string(dis_ip) + ":" + porthex2string(dis_port);
-
-            cout << inode << endl;
+            printConnection("tcp6", string(src_ip), src_port, string(dis_ip), dis_port, string(inode));
         }
     }
     else
@@ -102,12 +101,12 @@ void printIPV6Connections(string ipv6_path)
     }
 }
 
-void printConnections(string type, string filter)
+void getConnections(string type, string filter)
 {
     string ipv4_path = "/proc/net/" + type;
     string ipv6_path = "/proc/net/" + type + "6";
-    printIPV4Connections(ipv4_path);
-    printIPV6Connections(ipv6_path);
+    getIPV4Connections(ipv4_path);
+    getIPV6Connections(ipv6_path);
 }
 
 int main(int argc, char *argv[])
@@ -160,7 +159,7 @@ int main(int argc, char *argv[])
         cout << "List of TCP connections:" << endl;
         cout << left << setw(6) << "Proto" << setw(25) << "Local Address" << setw(25) << "Foreign Address"
              << "PID/Program name and arguments" << endl;
-        printConnections("tcp", filter_string);
+        getConnections("tcp", filter_string);
     }
     if (udp_switch)
     {
@@ -169,7 +168,7 @@ int main(int argc, char *argv[])
         cout << "List of UDP connections:" << endl;
         cout << left << setw(6) << "Proto" << setw(25) << "Local Address" << setw(25) << "Foreign Address"
              << "PID/Program name and arguments" << endl;
-        printConnections("udp", filter_string);
+        getConnections("udp", filter_string);
     }
 
     return 0;
