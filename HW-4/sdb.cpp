593349712a36c05125fbc94b352a7d882e3dea65
ballinhuang <x3562151@gmail.com>
2019-06-22 13:08:35 +0000

    6/22

--- HW-4/sdb.cpp
+++ HW-4/sdb.cpp
@@ -1,12 +1,15 @@
 #include <assert.h>
 #include <cstdlib>
+#include <cstdio>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/ptrace.h>
 #include <sys/user.h>
 #include <iostream>
+#include <cstring>
 
+#include "elftool.h"
 #include "ptools.h"
 
 using namespace std;
@@ -14,10 +17,12 @@ using namespace std;
 #define NOTLOADED 0
 #define LOADED 1
 #define RUNNING 2
-
 int status = 0;
+
 string programName = "";
 pid_t child;
+int wait_status;
+long long entry, offset, size;
 
 map<string, string> helpmap = {
     {"break", "break or b [loaded and running]: Setup a break point. If a program is loaded but is not running, the address should be within the range specified by the text segment in the ELF file. When a break point is hit, you have to output a message and indicate the corresponding address and instruction."},
@@ -51,6 +56,88 @@ void printHelp(string type)
 void load(string path)
 {
     programName = path;
+    int i;
+    elf_handle_t *eh = NULL;
+    elf_strtab_t *tab = NULL;
+    elf_init();
+
+    if ((eh = elf_open(programName.c_str())) == NULL)
+    {
+        fprintf(stderr, "** unabel to open '%s'.\n", programName.c_str());
+        return;
+    }
+
+    if (elf_load_all(eh) < 0)
+    {
+        fprintf(stderr, "** unable to load '%s.\n", programName.c_str());
+        goto quit;
+    }
+
+    for (tab = eh->strtab; tab != NULL; tab = tab->next)
+    {
+        if (tab->id == eh->shstrndx)
+            break;
+    }
+    if (tab == NULL)
+    {
+        fprintf(stderr, "** section header string table not found.\n");
+        goto quit;
+    }
+
+    for (i = 0; i < eh->shnum; i++)
+    {
+        if (strcmp(".text", &tab->data[eh->shdr[i].name]) == 0)
+        {
+            entry = eh->shdr[i].addr;
+            size = eh->shdr[i].size;
+            offset = eh->shdr[i].offset;
+            printf("** program '%s' loaded. entry point 0x%llx, vaddr 0x%llx, offset 0x%llx, size 0x%llx\n",
+                   programName.c_str(),
+                   eh->shdr[i].addr,
+                   eh->shdr[i].addr,
+                   eh->shdr[i].offset,
+                   eh->shdr[i].size);
+            break;
+        }
+    }
+quit:
+    if (eh)
+    {
+        elf_close(eh);
+        eh = NULL;
+    }
+    status = LOADED;
+}
+
+void vmmap()
+{
+    if (status == LOADED)
+    {
+        printf("%016llx-%016llx r-x %llx      %s\n", entry, entry + size, offset, programName.c_str());
+    }
+    else
+    {
+        map<range_t, map_entry_t> m;
+        map<range_t, map_entry_t>::iterator mi;
+        if (waitpid(child, &wait_status, 0) < 0)
+            errquit("waitpid");
+        ptrace(PTRACE_SETOPTIONS, child, 0, PTRACE_O_EXITKILL);
+        if (load_maps(child, m) > 0)
+        {
+            for (mi = m.begin(); mi != m.end(); mi++)
+            {
+                printf("## %lx-%lx %04o %s\n",
+                       mi->second.range.begin, mi->second.range.end,
+                       mi->second.perm, mi->second.name.c_str());
+            }
+            cout << "## " << m.size() << " map entries loaded.\n"
+                 << endl;
+        }
+    }
+}
+
+void start()
+{
     if ((child = fork()) < 0)
         errquit("fork");
     if (child == 0)
@@ -60,16 +147,20 @@ void load(string path)
         execlp(programName.c_str(), programName.c_str(), NULL);
         errquit("execvp");
     }
+    else
+    {
+        cout << "** pid " << child << endl;
+        status = RUNNING;
+    }
 }
 
 int main(int argc, char *argv[])
 {
-    /*
     if (argc > 1)
     {
-        programName = string(argv[1]);
+        load(string(argv[1]));
     }
-    */
+
     int count = 0;
     size_t pos = 0;
     string input;
@@ -77,6 +168,7 @@ int main(int argc, char *argv[])
     string args[2];
     while (true)
     {
+        cout << "sdb> ";
         count = 0;
         getline(cin, input);
         while ((pos = input.find(' ')) != std::string::npos)
@@ -119,7 +211,32 @@ int main(int argc, char *argv[])
                 continue;
             }
         }
-
+        else if (cmd == "start")
+        {
+            if (status == LOADED)
+            {
+                start();
+                continue;
+            }
+            else
+            {
+                printHelp("start");
+                continue;
+            }
+        }
+        else if (cmd == "vmmap" || cmd == "m")
+        {
+            if (status == LOADED || status == RUNNING)
+            {
+                vmmap();
+                continue;
+            }
+            else
+            {
+                printHelp("vmmap");
+                continue;
+            }
+        }
         else if (cmd == "break" || cmd == "b")
         {
             if (status == LOADED || status == RUNNING)
@@ -136,5 +253,5 @@ int main(int argc, char *argv[])
             cout << "bye~" << endl;
             exit(0);
         }
-    }
+        }
 }
